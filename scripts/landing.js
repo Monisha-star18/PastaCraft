2fe7b75b054b96f344fc11b9de5705bd71ba8dfc
Monisha-star18 <monisha.r180505@gmail.com>
2026-08-05 11:00:21 +0530

    page-flip book component using StPageFlip structure is created

--- scripts/landing.js
+++ scripts/landing.js
@@ -12,3 +12,19 @@ $(window).on("scroll",function ()
             $("nav").removeClass("change-bg");
         }
     })
+
+//book flip
+
+const pageFlip = new St.PageFlip(document.getElementById('book') , {
+    width :350 , height:450,
+    size : "fixed" , 
+    showCover :true,
+    useProtrait : false,
+    useMouseEvents : false,
+    drawShadow :true,
+})
+
+pageFlip.loadFromHTML(document.querySelectorAll('#book .page'))
+
+document.getElementById('preBtn').addEventListener('click',()=> { pageFlip.flipPrev()})
+document.getElementById('nextBtn').addEventListener('click',()=> { pageFlip.flipNext()})
