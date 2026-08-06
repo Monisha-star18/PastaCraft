a9fac05d54e1f988646eed1735fe156b38f77bea
Monisha-star18 <monisha.r180505@gmail.com>
2026-08-06 17:30:25 +0530

    changed heading

---
+++ scripts/experience.js
@@ -0,0 +1,13 @@
+
+$(".music-img").click(function() {
+    
+    // 1. Remove the glow 
+    $(".music-img").not(this).removeClass("music-click");
+    
+    // 2. Toggle the glow on the clicked button
+    $(this).toggleClass("music-click");
+});
+
+$("#level1-frame").click(function(){
+    window.location.href = "../pages/gamePage-Level1.html"
+})
