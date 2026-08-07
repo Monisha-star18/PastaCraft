cbf84efd4eed580bc36d8e82bbf46d8a812c8b06
Monisha-star18 <monisha.r180505@gmail.com>
2026-08-07 14:47:41 +0530

    added correct functioanlity of steps

--- scripts/gamePage-Level1.js
+++ scripts/gamePage-Level1.js
@@ -1,27 +1,38 @@
 
 $(document).ready(function () {
 
+    let stepCount = 1;
+    let eggClickCount =0 ; 
+
     $("#whiteFlour").click(function(){
-        $("#step1-div").css("display", "block");
+        if(stepCount ==  1)
+        {
+            $("#step1-div").css("display", "block");
+        }
+        stepCount ++ ;
+        eggClickCount =0 ;
     })
 
-    let eggClickCount =0 ; 
+    
 
 
     $("#eggs").click(function(){
 
         eggClickCount ++ ;
+        console.log(eggClickCount)
         
-        if(eggClickCount == 1)
+        if(eggClickCount == 1 && stepCount ==  2)
         {
             $("#step1-div").css("display", "none");
             $("#step2-div").css("display", "block");
+            stepCount ++ ;
         }
 
-        else if(eggClickCount ==2)
+        else if(eggClickCount ==2 && stepCount ==  3)
         {
             $("#step2-div").css("display", "none");
             $("#step3-div").css("display", "block");
+            stepCount ++ ;
         }
 
     })
