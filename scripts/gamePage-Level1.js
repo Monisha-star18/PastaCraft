2b42f14997230fd6ad1ea546886a3ca005c483d5
Monisha-star18 <monisha.r180505@gmail.com>
2026-08-07 14:34:43 +0530

    js for pasta making functionality added

--- scripts/gamePage-Level1.js
+++ scripts/gamePage-Level1.js
@@ -1,4 +1,29 @@
 
-$("#eggs").click(function(){
-    $("#step1-div").addClass('show');
-});
+$(document).ready(function () {
+
+    $("#whiteFlour").click(function(){
+        $("#step1-div").css("display", "block");
+    })
+
+    let eggClickCount =0 ; 
+
+
+    $("#eggs").click(function(){
+
+        eggClickCount ++ ;
+        
+        if(eggClickCount == 1)
+        {
+            $("#step1-div").css("display", "none");
+            $("#step2-div").css("display", "block");
+        }
+
+        else if(eggClickCount ==2)
+        {
+            $("#step2-div").css("display", "none");
+            $("#step3-div").css("display", "block");
+        }
+
+    })
+
+})
