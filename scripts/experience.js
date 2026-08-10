30e609bf8cccd6165adb8cb85aa0983a946126ea
Monisha-star18 <monisha.r180505@gmail.com>
2026-08-10 18:57:13 +0530

    complted the level 1

--- scripts/experience.js
+++ scripts/experience.js
@@ -15,16 +15,31 @@ $("#level1-frame").click(function(){
     window.location.href = "../pages/gamePage-Level1.html"
 })
 
+$("#level2-frame").click(function(){
+    window.location.href = "../pages/gamePage-Level2.html"
+})
+
 $(".exit").click(function(){
     window.location.href = "../index.html"
     localStorage.removeItem("level");
 })
 
-let level = localStorage.getItem("level")
+let level = Number(localStorage.getItem("level"))
 
-if (level == 2)
+
+switch(level)
 {
-    $("#level-cover-2").css("display","none")
+    case 2 :
+        $("#level-cover-2").css("display","none");
+        break;
+
+    case 3 :
+        $("#level-cover-3").css("display","none");
+        break;
+
+    case 4 :
+        $("#level-cover-4").css("display","none");
+        break;
 }
 
 // const query = "Ratatouille Pasta"
