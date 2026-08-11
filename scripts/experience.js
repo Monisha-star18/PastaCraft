bbcaebbd960d39659906cf25523ab7fd05b01f74
Monisha-star18 <monisha.r180505@gmail.com>
2026-08-11 11:13:56 +0530

    last chnages

--- scripts/experience.js
+++ scripts/experience.js
@@ -41,31 +41,3 @@ switch(level)
         $("#level-cover-4").css("display","none");
         break;
 }
-
-// const query = "Ratatouille Pasta"
-// async function testAPIKey() {
-//     const apiKey = '5d680de750cd428bbd866e20f5bf0d85';
-//     // Simple test - just get 1 recipe
-//     const testUrl = `https://api.spoonacular.com/recipes/complexSearch?query=${query}&number=3&apiKey=${apiKey}`;
-    
-//     try {
-//         const response = await fetch(testUrl);
-//         console.log('Status:', response.status);
-//         console.log('Status Text:', response.statusText);
-        
-//         if (!response.ok) {
-//             const errorData = await response.text();
-//             console.log('Error Response:', errorData);
-//             throw new Error(`Status ${response.status}: ${errorData}`);
-//         }
-        
-//         const data = await response.json();
-//         console.log('Success!', data);
-//         return data;
-//     } catch (error) {
-//         console.error('Test failed:', error);
-//     }
-// }
-
-// // Run this in your browser console
-// testAPIKey();
